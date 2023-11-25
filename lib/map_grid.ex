de3440fc81310888addf3be394f3f451f8af4522
Joeri Dijkstra <joeri.dijkstra@bettyblocks.com>
2023-11-25 13:18:37 +0100

    chore: initial commit

---
+++ lib/map_grid.ex
@@ -0,0 +1,18 @@
+defmodule MapGrid do
+  @moduledoc """
+  Documentation for `MapGrid`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MapGrid.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/map_grid_test.exs
@@ -0,0 +1,8 @@
+defmodule MapGridTest do
+  use ExUnit.Case
+  doctest MapGrid
+
+  test "greets the world" do
+    assert MapGrid.hello() == :world
+  end
+end
