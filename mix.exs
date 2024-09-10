4fd904c0f1e475e6b6c262579a24a8f9d99ee7b1
Joeri <joeri.dijkstra@bettyblocks.com>
2024-09-10 08:43:20 +0200

    chore: bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule MapGrid.MixProject do
   def project do
     [
       app: :map_grid,
-      version: "1.0.2",
+      version: "1.1.0",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
